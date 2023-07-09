24152771d9da1e8c03da8f765ccd411ed21bcf19
Yong <boxone3840@naver.com>
2023-07-09 21:29:04 +0900

    monaco적용 제외 탭생성 완료

--- monaco.js
+++ monaco.js
@@ -1,34 +1,34 @@
 var editor;
 
 require.config({
-    paths: {
-        vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.39.0/min/vs",
-    },
+  paths: {
+    vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.39.0/min/vs",
+  },
 });
 
 require(["vs/editor/editor.main"], function () {
-    editor = monaco.editor.create(document.getElementById("monaco"), {
-        theme: "vs-dark",
-        automaticLayout: true,
-        language: "c",
-        value: ["int main() {", "", "}"].join("\n"),
-    });
+  editor = monaco.editor.create(document.querySelector(".monaco"), {
+    theme: "vs-dark",
+    automaticLayout: true,
+    language: "c",
+    value: ["int main() {", "", "}"].join("\n"),
+  });
 });
 
 var myModalEl = document.getElementById("staticBackdrop");
 
 myModalEl.addEventListener("show.bs.modal", function (event) {
-    var modalCode = document.querySelector(".modal-code");
-    var editorCode = editor.getValue();
+  var modalCode = document.querySelector(".modal-code");
+  var editorCode = editor.getValue();
 
-    var editorReadOnly = monaco.editor.create(
-        document.getElementById("monaco-read-only"),
-        {
-            theme: "vs-dark",
-            automaticLayout: true,
-            language: "c",
-            value: editorCode,
-            readOnly: true,
-        }
-    );
+  var editorReadOnly = monaco.editor.create(
+    document.getElementById("monaco-read-only"),
+    {
+      theme: "vs-dark",
+      automaticLayout: true,
+      language: "c",
+      value: editorCode,
+      readOnly: true,
+    }
+  );
 });
--- tab.js
+++ tab.js
@@ -2,17 +2,13 @@ var editor;
 var tabCount = 2; // 탭 카운트 변수 초기화
 
 window.onload = function () {
-  $("#table1").show();
-  $("#table2").hide();
-  $("#table3").hide();
-  $("#table4").hide();
+  $("#tab1").show();
+  $("#tab_console").hide();
 };
 $(document).ready(function () {
   $("#index_tab1").click(function () {
-    $("#table1").show();
-    $("#table2").hide();
-    $("#table3").hide();
-    $("#table4").hide();
+    $(".codemate-content > div").hide();
+    $("#tab1").show();
   });
   $("#append").click(function () {
     var tabName;
@@ -23,26 +19,35 @@ $(document).ready(function () {
     } while (tabName && !isValidTabName(tabName)); // 유효한 탭 이름이 아닐 경우 반복해서 입력 받기
 
     if (tabName) {
-      var tabID = "index_tab" + tabCount; // 탭의 고유 ID 생성
+      var tabID = "tab" + tabCount; // 탭의 고유 ID 생성
+
       var newButton = $("<button>")
         .attr("type", "button")
-        .attr("id", tabID)
+        .attr("id", "index_" + tabID)
         .text(tabName);
-      $("#index_tab1").after(newButton); // 탭 버튼을 Default.c 버튼과 + 버튼 사이에 삽입
+      $("#append").before(newButton); // 탭 버튼을 Default.c 버튼과 "+" 버튼 사이에 삽입
+
+      // 기존 탭 숨김 처리
+      $(".codemate-content > div").hide();
+
+      var newTabDiv = $("<div>").attr("id", tabID).show(); // 새로운 탭의 div 태그 생성
+      var newEditorDiv = $("<div>").addClass("monaco").css("height", "600px"); // 탭 내용을 표시할 div 태그 생성
+      newTabDiv.append(newEditorDiv).append("hello"); // 탭 div에 내용 div 추가       //확인을 위해서 hello추가 //현재 div가 생성은 되지만 monaco가 적용되지 않음
+      $("#tab_console").before(newTabDiv); // 탭 div를 "tab_console" 태그 위에 추가
+
+      // 새로운 탭 클릭 시 해당 탭 보여주기
+      newButton.click(function () {
+        $(".codemate-content > div").hide();
+        newTabDiv.show();
+      });
+
       tabCount++; // 탭 카운트 증가
     }
   });
+
   $("#index_console").click(function () {
-    $("#table1").hide();
-    $("#table2").hide();
-    $("#table3").show();
-    $("#table4").hide();
-  });
-  $("#ai").click(function () {
-    $("#table1").hide();
-    $("#table2").hide();
-    $("#table3").hide();
-    $("#table4").show();
+    $(".codemate-content > div").hide();
+    $("#tab_console").show();
   });
 });
 
